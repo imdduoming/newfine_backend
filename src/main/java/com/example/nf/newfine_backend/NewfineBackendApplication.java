220b8e73d5fdd0bfee28327b61124e55ec023bef
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-05-11 19:46:05 +0900

    자바 기본

---
+++ src/main/java/com/example/nf/newfine_backend/NewfineBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nf.newfine_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewfineBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewfineBackendApplication.class, args);
+    }
+
+}
