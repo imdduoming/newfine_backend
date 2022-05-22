1f63a67b5feef3dea2d6b8462aa94f852eb2048d
dduoming <dltndudvldzm@naver.com>
2022-05-22 18:43:57 +0900

    erd entity 설계

---
+++ src/main/java/com/example/nf/newfine_backend/TTest.java
@@ -0,0 +1,36 @@
+package com.example.nf.newfine_backend;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@Entity
+public class TTest {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    @Column(name = "course_id")
+    private Long TteId;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = { CascadeType.PERSIST})
+    private Teacher teacher;
+
+
+    @OneToMany(mappedBy="course", cascade = { CascadeType.PERSIST,CascadeType.REMOVE})
+    private List<STest> sTests;
+
+    @Column(nullable = false)
+    private Date entranceTime;
+
+    @Column(nullable = false)
+    private Date endTime;
+
+
+}
