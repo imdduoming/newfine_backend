dd6a8edbf84f52eedcb62e942e726662df6307d1
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-07-07 11:42:23 +0900

    post 404 error fix #7

--- src/main/java/com/example/nf/newfine_backend/attendance/AttendanceController.java
+++ src/main/java/com/example/nf/newfine_backend/attendance/AttendanceController.java
@@ -5,6 +5,7 @@ import com.example.nf.newfine_backend.StudentRepostiory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.tags.Param;
 
 import java.util.List;
 
@@ -15,13 +16,13 @@ public class AttendanceController {
     private final AttendanceService attendanceService;
     private final StudentRepostiory studentRepostiory;
 
-    @PostMapping (value = "/add/attendance" )
-    public Attendance addAttendance(@RequestParam@RequestBody AttendanceDto attendanceDto) {
+    @PostMapping (value = "add/attendance" )
+    public Attendance addAttendance(@RequestBody AttendanceDto attendanceDto) {
 
         return attendanceService.addAttendance(attendanceDto);
 
     }
-    @GetMapping("/get/all/attendances")
+    @GetMapping("get/all/attendances")
     public List<Attendance> getAllAttendances(){
         return attendanceService.getAllAttendances();
     }
