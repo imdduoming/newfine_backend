645f7c8a2eb1b9c85a70edc17afbe2001ecf6be7
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-07-19 00:49:41 +0900

    Listenr entity - student , course 다대다 관계 해결

---
+++ src/main/java/com/example/nf/newfine_backend/course/ListenerRepository.java
@@ -0,0 +1,6 @@
+package com.example.nf.newfine_backend.course;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ListenerRepository extends JpaRepository<Listener,Long> {
+}
