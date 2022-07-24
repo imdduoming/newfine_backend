74119c50d291739e4f44ab6aafbf4a53fe733335
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-07-25 02:20:16 +0900

    index.html 접근 허용

--- src/main/java/com/example/nf/newfine_backend/student/config/SecurityConfig.java
+++ src/main/java/com/example/nf/newfine_backend/student/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {  // Spring Se
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-                .antMatchers("/h2-console/**", "/favicon.ico","/js/**");
+                .antMatchers("/h2-console/**", "/favicon.ico","/js/**","/resources/**");
     }
 
     @Override
