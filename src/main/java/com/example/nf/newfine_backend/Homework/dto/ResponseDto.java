e1eb76904dcb04a2224917b7f81064d868bcb85f
yuna4490 <67116186+yuna4490@users.noreply.github.com>
2022-07-27 11:08:30 +0200

    3nd commit

---
+++ src/main/java/com/example/nf/newfine_backend/Homework/dto/ResponseDto.java
@@ -0,0 +1,34 @@
+package com.example.nf.newfine_backend.Homework.dto;
+
+import com.example.nf.newfine_backend.Homework.domain.THomework;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ResponseDto {
+
+    private Long id; // PK
+    private String title; // 제목
+    private String content; // 내용
+    private String writer; // 작성자
+    private int count; // 조회 수
+    //private char deleteYn; // 삭제 여부
+    private String createdDate; // 생성일
+    private String modifiedDate; // 수정일
+
+
+    public ResponseDto(THomework entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.writer = entity.getWriter();
+        this.count = entity.getCount();
+        this.createdDate = entity.getCreatedDate();
+        this.modifiedDate = entity.getModifiedDate();
+        // 엔티티간 무한참조 방지
+    }
+
+}
