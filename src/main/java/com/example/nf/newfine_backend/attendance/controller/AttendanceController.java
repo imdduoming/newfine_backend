707117a9c27b473b87c8d116436488b367a7bed4
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-07-30 21:47:31 +0900

    로그인 연

--- src/main/java/com/example/nf/newfine_backend/attendance/controller/AttendanceController.java
+++ src/main/java/com/example/nf/newfine_backend/attendance/controller/AttendanceController.java
@@ -14,8 +14,11 @@ import com.example.nf.newfine_backend.student.repository.StudentRepository;
 import com.example.nf.newfine_backend.student.util.SecurityUtil;
 import com.sun.tools.jconsole.JConsoleContext;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.jni.Local;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.web.header.Header;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -26,6 +29,7 @@ import java.util.Optional;
 @RestController
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@Slf4j
 public class AttendanceController {
     private final AttendanceService attendanceService;
     private final CourseRepository courseRepository;
@@ -45,7 +49,8 @@ public class AttendanceController {
 }
     // 학생 출석 api
     @PostMapping  (value = "/add/attendance" )
-    public int addAttendance(@RequestBody StudentAttendanceDto studentAttendanceDto) {
+    public int addAttendance(@RequestBody StudentAttendanceDto studentAttendanceDto, @RequestHeader HttpHeaders headers) {
+        log.info("token",headers.toSingleValueMap().toString());
         Long attendance_id=Long.parseLong(studentAttendanceDto.getAttendance_id());
         Student student=studentRepository.findById(SecurityUtil.getCurrentMemberId()).orElseThrow(PhoneNumberNotFoundException::new);
         int ans=attendanceService.addAttendance(attendance_id,student);
