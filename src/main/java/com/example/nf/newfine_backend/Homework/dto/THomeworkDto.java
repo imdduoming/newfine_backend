c3649c4eeb3a7168f8c35b1716d34a49d7e52e60
yuna4490 <67116186+yuna4490@users.noreply.github.com>
2022-08-01 22:26:11 +0200

    4nd commit

--- src/main/java/com/example/nf/newfine_backend/Homework/dto/THomeworkDto.java
+++ src/main/java/com/example/nf/newfine_backend/Homework/dto/THomeworkDto.java
@@ -2,7 +2,6 @@ package com.example.nf.newfine_backend.Homework.dto;
 
 
 import com.example.nf.newfine_backend.Homework.domain.THomework;
-import com.example.nf.newfine_backend.course.Course;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,7 +14,6 @@ public class THomeworkDto {
     private Long id; // PK
     private String title; // 제목
     private String content; // 내용
-    private Course course; // 작성자
     //private int count; // 조회 수
     //private char deleteYn; // 삭제 여부
     private String createdDate; // 생성일
@@ -27,7 +25,6 @@ public class THomeworkDto {
                 tHomework.getId(),
                 tHomework.getTitle(),
                 tHomework.getContent(),
-                tHomework.getCourse(),
                 tHomework.getCreatedDate(),
                 tHomework.getModifiedDate()
         );
