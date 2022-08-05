5902196ca5c0f58885a95c2f51b2bb56cd0ab4ee
wanzaS2 <moono0129@daum.net>
2022-08-05 19:38:10 +0900

    StudentTestResults 주석 처리..

--- src/main/java/com/example/nf/newfine_backend/test/dto/StudentTestResultsDto.java
+++ src/main/java/com/example/nf/newfine_backend/test/dto/StudentTestResultsDto.java
@@ -1,13 +1,12 @@
-package com.example.nf.newfine_backend.test.dto;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-@Setter
-@Getter
-@NoArgsConstructor
-@AllArgsConstructor
-public class StudentTestResultsDto {
-}
+//package com.example.nf.newfine_backend.test.dto;
+//
+//import lombok.AllArgsConstructor;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//
+//@Setter
+//@Getter
+//@NoArgsConstructor
+//public class StudentTestResultsDto {
+//}
