249e1ccf40f554ff23eba4e79d6f70c751c2c608
wanzaS2 <moono0129@daum.net>
2022-08-06 20:54:06 +0900

    웹 엑셀파일 업로드 페이지 수정

--- src/main/java/com/example/nf/newfine_backend/test/controller/TestController.java
+++ src/main/java/com/example/nf/newfine_backend/test/controller/TestController.java
@@ -1,28 +1,26 @@
 package com.example.nf.newfine_backend.test.controller;
 
-import com.example.nf.newfine_backend.admin.dto.DeleteRequestByAdminDto;
-import com.example.nf.newfine_backend.attendance.domain.Attendance;
-import com.example.nf.newfine_backend.attendance.dto.AttendanceDto;
 import com.example.nf.newfine_backend.test.domain.Test;
 import com.example.nf.newfine_backend.test.dto.TestDto;
 import com.example.nf.newfine_backend.test.service.TestService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 @Controller
 @AllArgsConstructor
-public class testController {
+public class TestController {
 
     private final TestService testService;
 
     @PostMapping(value = "/test/create")
     public Test createTest(HttpServletRequest request, TestDto testDto) {
+        System.out.println(request.getParameter("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"+"test_date"));
+        System.out.println(request.getParameter("course_id")+"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        testDto.setCourse_id(Long.valueOf(request.getParameter("course_id")));
         testDto.setTestDate(LocalDate.parse(request.getParameter("test_date")));
         testDto.setTestName(request.getParameter("test_name"));
         testDto.setCourse_id(Long.valueOf(request.getParameter("course_id")));
