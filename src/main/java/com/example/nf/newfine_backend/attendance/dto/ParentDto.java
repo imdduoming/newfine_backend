f019fa4e678bb88b5fbe37db4b3284109068fd1b
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-08-07 00:37:06 +0900

    부모님 번호 받아오기

---
+++ src/main/java/com/example/nf/newfine_backend/attendance/dto/ParentDto.java
@@ -0,0 +1,14 @@
+package com.example.nf.newfine_backend.attendance.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ParentDto {
+    private String phoneNumber;
+}
