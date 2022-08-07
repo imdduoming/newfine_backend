29e624bd03757e56e2727e9ee724a56a08dc2ef2
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-08-07 17:43:28 +0900

    티쳐 edit 로그인연동

--- src/main/java/com/example/nf/newfine_backend/member/config/SecurityConfig.java
+++ src/main/java/com/example/nf/newfine_backend/member/config/SecurityConfig.java
@@ -64,7 +64,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {  // Spring Se
                 .antMatchers("/all/**").permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/make/attendance").permitAll()
-                .antMatchers("/get/all/courses").permitAll()
+                .antMatchers("/get/**").permitAll()
                 .anyRequest().authenticated()   // 나머지 API 는 전부 인증 필요
 
                 // JwtFilter 를 addFilterBefore 로 등록했던 JwtSecurityConfig 클래스를 적용
