e3559fe14643dc52176663f0fbf49b5d45743db0
wanzaS2 <moono0129@daum.net>
2022-08-11 01:16:19 +0900

    로그인 시 에러 메세지 수정

--- src/main/java/com/example/nf/newfine_backend/member/service/AuthService.java
+++ src/main/java/com/example/nf/newfine_backend/member/service/AuthService.java
@@ -81,7 +81,6 @@ public class AuthService {
     // 로그인 예외처리**
     @Transactional
     public TokenDto login(SignInDto signInDto) {
-
         // 비밀번호 확인 추가
         if (teacherRepository.existsByPhoneNumber(signInDto.getPhoneNumber())){
             Teacher teacher=teacherRepository.findByPhoneNumber(signInDto.getPhoneNumber()).orElseThrow(()->new PhoneNumberNotFoundException("회원 정보가 없습니다.\n회원가입을 먼저 해주세요."));
