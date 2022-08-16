2993b11fe3959123a63eff09be853b010023dac9
wanzaS2 <moono0129@daum.net>
2022-08-17 03:25:08 +0900

    테스트 코드 일단 끈ㅌ...

--- src/main/java/com/example/nf/newfine_backend/test/domain/ScienceDetailCode.java
+++ src/main/java/com/example/nf/newfine_backend/test/domain/ScienceDetailCode.java
@@ -16,15 +16,15 @@ public enum ScienceDetailCode {
     public static String generateScienceSubjectCode(String detailSubject){
         switch (detailSubject){
             case "화학":
-                return "1"+화학.scienceDetailCode();
+                return "S"+화학.scienceDetailCode();
             case "물리":
-                return "1"+물리.scienceDetailCode();
+                return "S"+물리.scienceDetailCode();
             case "생명과학":
-                return "1"+생명과학.scienceDetailCode();
+                return "S"+생명과학.scienceDetailCode();
             case "지구과학":
-                return "1"+지구과학.scienceDetailCode();
+                return "S"+지구과학.scienceDetailCode();
             default:
-                return "1"+공통과학.scienceDetailCode();
+                return "S"+공통과학.scienceDetailCode();
         }
     }
 }
