e5b784da3fba0ac0ce3e09dccec8af6f9e52e29b
wanzaS2 <moono0129@daum.net>
2022-08-22 06:40:13 +0900

    등급 변경 일단 12시간마다

--- src/main/java/com/example/nf/newfine_backend/Scheduler.java
+++ src/main/java/com/example/nf/newfine_backend/Scheduler.java
@@ -12,7 +12,8 @@ import org.springframework.stereotype.Component;
 public class Scheduler {
     private final RankingService rankingService;
 
-    @Scheduled(cron = "0 */5 * * * *")	// 5분마다
+//    @Scheduled(cron = "0 */5 * * * *")	// 5분마다
+    @Scheduled(cron = "0 */12 * * *")	// 12시간마다
     public void updateLevel() throws Exception {
 //        if(!env.equals("prod")) {
 //            log.info("매일 5분마다 - " + env);
