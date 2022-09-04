62a59ddf7c7c92cd1bda47216525b88f96a3fa32
dduoming <60255575+imdduoming@users.noreply.github.com>
2022-09-04 12:38:52 +0900

    websecurity config /study , /attendance 허용

--- src/main/java/com/example/nf/newfine_backend/member/config/SecurityConfig.java
+++ src/main/java/com/example/nf/newfine_backend/member/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {  // Spring Se
                 .and()
                 .authorizeRequests()
 //                .antMatchers("/", "/**").permitAll()
-                .antMatchers("/", "/adminLogin", "/attendanceMake", "/main", "/studentInfo", "/studyMake", "/testUpload").permitAll()
+                .antMatchers("/", "/adminLogin", "/attendanceMake", "/main", "/studentInfo", "/studyMake", "/testUpload","/study","/attendance").permitAll()
                 .antMatchers("/all/**").permitAll()
                 .antMatchers("/auth/**").permitAll()
 //                .antMatchers("/make/attendance").permitAll()
